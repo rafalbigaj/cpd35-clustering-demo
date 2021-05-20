2368ed175b651000faa79393b9da44701461897f
Demo User <demo@ibm.com>
2021-05-20 07:15:20 +0000

    Data assets

--- assets/jupyterlab/scoring.py
+++ assets/jupyterlab/scoring.py
@@ -6,24 +6,27 @@ import os
 import sys
 
 
-def find_script_dir():
+def find_project_dir():
     script_dir = None
     try: script_dir = os.path.dirname(os.path.realpath(__file__))
-    except NameError: script_dir = os.path.realpath("project_git_repo/cpd35-clustering-demo/assets/jupyterlab")
+    except NameError: script_dir = os.path.realpath("project_git_repo/cpd35-clustering-demo")
         
     return script_dir
 
 
-SCRIPT_DIR = find_script_dir()
-print(SCRIPT_DIR)
+PROJECT_DIR = find_project_dir()
+SCRIPT_DIR = os.path.join(PROJECT_DIR, "assets/jupyterlab")
+DATA_DIR = os.path.join(PROJECT_DIR, "assets/data_asset")
 sys.path.append(os.path.normpath(SCRIPT_DIR))
+print(SCRIPT_DIR)
+print(DATA_DIR)
 
 # from special_score.cpu_calculation import super_complexs_score
 # from special_score.gpu_calculation import good_neigbour
 from special_score.dbscan import my_dbscan
 from preprocess_data import one_hot_encoder
 
-reference_df = pd.read_csv(os.path.join(SCRIPT_DIR, "data/credit_risk_reference.csv"))
+reference_df = pd.read_csv(os.path.join(DATA_DIR, "credit_risk_reference.csv"))
 input_df = reference_df.drop(['Risk'], axis=1)
 
 transformer = one_hot_encoder(input_df)
@@ -39,3 +42,4 @@ model = pipeline_linear.fit(input_df)
 # buddy = good_neigbour(trades_prepared_filtered_df['account', 'date', 'volume'])
 
 print(np.unique(dbscan_model.labels_))
+    
