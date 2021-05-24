2d005f77d5b757d4f86cbd454b5b2a977d70a5b4
rafal-bigaj <rafal.bigaj@pl.ibm.com>
2021-05-24 14:14:07 +0200

    jenkins pipelines: deploy (Production)

--- scripts/test_run.sh
+++ scripts/test_run.sh
@@ -4,10 +4,7 @@ export CPDCTL_ENABLE_CODE_PACKAGE=1
 cpdctl config context use cpd_prod
 
 prod_space_id=$PROD_SPACE_ID
+prod_job_id=$PROD_JOB_ID
 
-# Get ID of job for the existing code package
-prod_job_id=$(cpdctl job list --space-id $prod_space_id --asset-ref-type code_package --raw-output --output json -j 'results[0].metadata.asset_id')
-
-echo "Job ID: $prod_job_id"
-
+# Trigger job run
 cpdctl job run create --space-id $prod_space_id --job-id $prod_job_id --job-run '{}'
